171687e567f822cc4bb88d4a29389bc08226c7db
coela-oss <to@coela.org>
2025-06-28 18:39:54 +0900

    Fix UI Structure

---
+++ nuxt-app/app/components/global/Header.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+
+    <Navigation></Navigation>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- nuxt-app/app/pages/index.vue
+++ nuxt-app/app/pages/index.vue
@@ -1,6 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+definePageMeta({
+  layout: false
+})
+</script>
 <template>
   <div>
+    <canvas></canvas>
     <NuxtLink to="products">link</NuxtLink>
   </div>
 </template>
